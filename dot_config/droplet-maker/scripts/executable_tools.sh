738aa8d041e1dc10f9b250ffaca5a384c4a87c3b
Preetham Gujjula <gitcommit@mail.preetham.io>
2024-10-02 01:02:30 -0700

    [droplet-maker] Add rclone installation

--- dot_config/droplet-maker/scripts/executable_tools.sh
+++ dot_config/droplet-maker/scripts/executable_tools.sh
@@ -51,6 +51,23 @@ cd fonts
 cd ..
 rm -rfv fonts
 
+### Install rclone
+curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+unzip rclone-current-linux-amd64.zip
+cd rclone-*-linux-amd64
+
+sudo cp rclone /usr/local/bin/
+sudo chown root:root /usr/local/bin/rclone
+sudo chmod 755 /usr/local/bin/rclone
+
+sudo mkdir -p /usr/local/share/man/man1
+sudo cp rclone.1 /usr/local/share/man/man1/
+sudo mandb
+
+cd ..
+rm -rfv rclone-current-linux-amd64.zip
+rm -rfv rclone-*-linux-amd64
+
 ### Allow X11 Forwarding
 echo "X11Forwarding yes" | sudo tee -a /etc/ssh/sshd_config
 touch ~pgujjula/.Xauthority
